e499bb3e7b2886a56401d01d2073d78817a66e0a
DancingSnow <1121149616@qq.com>
2023-05-20 23:16:46 +0800

    更新获取下载链接脚本

--- docs/scripts/download_link.js
+++ docs/scripts/download_link.js
@@ -4,22 +4,27 @@ function set_download_url() {
     httpRequest.send();
     httpRequest.onreadystatechange = function () {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            let download_url
+            let download_url = 'https://github.com/DancingSnow0517/StarRail-gacha/releases'
+            let version_tag = null
             if (httpRequest.status === 200) {
                 try {
                     let json = JSON.parse(httpRequest.responseText);
-                download_url = json.assets[0].browser_download_url;
+                    download_url = json.assets[0].browser_download_url;
+                    version_tag = json.tag_name
                 } catch (e) {
                     console.log('获得下载链接失败.');
-                    download_url = 'https://github.com/DancingSnow0517/StarRail-gacha/releases'
                 }
 
             } else {
                 console.log('获得下载链接失败.');
-                download_url = 'https://github.com/DancingSnow0517/StarRail-gacha/releases'
             }
             let bt1 = document.getElementById('download_1')
             let bt2 = document.getElementById('download_2')
+            if (version_tag === null) {
+                bt1.innerText = '立即下载'
+            } else {
+                bt1.innerText = '立即下载 v.' + version_tag
+            }
             bt1.href = download_url
             bt2.href = download_url
         }
